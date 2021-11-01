2d49c5e1acf7b5d5c716a661c0dda84e30161642
nomekop007 <d.riosrojas007@gmail.com>
2021-10-31 22:02:58 -0300

    cambio de url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://ballet-tatitacumi.herokuapp.com/';
+$config['base_url'] = 'http://www.ballettalitacumi.com/';
 
 /*
 |--------------------------------------------------------------------------
